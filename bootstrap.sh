43f264640f8a617c036c3a357ac0bd1335eac7f7
Joey Zhou <josephyzhou@gmail.com>
2014-03-12 01:18:39 -0700

    always pull submodule

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,4 @@
+git submodule foreach git pull
 protoc --descriptor_set_out=lib/api/protos/bitcoinserver.desc --include_imports lib/api/protos/bitcoinserver.proto 
 protoc --descriptor_set_out=lib/api/protos/bitcoin.desc --include_imports lib/api/protos/bitcoin.proto 
 protoc --descriptor_set_out=lib/api/protos/paymentrequest.desc --include_imports lib/api/protos/paymentrequest.proto 
